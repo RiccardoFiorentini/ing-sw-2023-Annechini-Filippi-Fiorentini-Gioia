c3e88c428ace821c880fe47f8e17ceaba33fc665
NicoleFilippi <nicole.filippi@mail.polimi.it>
2023-03-22 16:50:24 +0100

    UML update; class structure of: Chat, Model

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/Model/Chat.java
@@ -0,0 +1,18 @@
+package main.java.it.polimi.ingsw.Model;
+
+import java.util.List;
+public class Chat {
+    private List<Message> messages;
+    public Chat(){
+
+    }
+    public void writeMessage(Player sender, Player receiver, String text){
+
+    }
+    public void writeMessage(Player sender, String text){
+
+    }
+    public List<Message> getMessages() {
+
+    }
+}
---
+++ MyShelfie/src/main/java/it/polimi/ingsw/Model/Model.java
@@ -0,0 +1,60 @@
+package main.java.it.polimi.ingsw.Model;
+
+import java.util.List;
+public class Model {
+    private int gameId;
+    private int numPlayers;
+    private List<Player> players;
+    private Board board;
+    private int turnId;
+    private Player firstToEnd;
+    private Player firstToStart;
+    private CommonGoal[] commonGoals;
+    private List<Integer> finalPoints;
+    private Chat chat;
+    public Model(int gameId, List<Player> players){
+
+    }
+    public void nextTurn(){
+
+    }
+    public void addPlayer(Player player){
+
+    }
+    public void startGame(){
+
+    }
+    public boolean equalsTiles(Tile t1, Tile t2){
+
+    }
+    public int getGameId() {
+
+    }
+    public int getNumPlayers() {
+
+    }
+    public List<Player> getPlayers() {
+
+    }
+    public Board getBoard() {
+
+    }
+    public int getTurnId() {
+
+    }
+    public Player getFirstToEnd() {
+
+    }
+    public Player getFirstToStart() {
+
+    }
+    public CommonGoal[] getCommonGoals() {
+
+    }
+    public List<Integer> getFinalPoints() {
+
+    }
+    public Chat getChat() {
+
+    }
+}
