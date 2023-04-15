4c221c66ec0ce4ed99d8df1f417c0bbacf211738
Pasquale Gioia <74321370+pasqualegioia@users.noreply.github.com>
2023-04-15 13:25:00 +0200

    MVC Sequence Diagram; updated BoardTest

--- MyShelfie/src/test/ModelTest/BoardTest.java
+++ MyShelfie/src/test/ModelTest/BoardTest.java
@@ -4,7 +4,7 @@ import main.java.it.polimi.ingsw.Model.Board;
 import main.java.it.polimi.ingsw.Model.Tile;
 import main.java.it.polimi.ingsw.ModelExceptions.NotPickableException;
 import main.java.it.polimi.ingsw.ModelExceptions.NotToRefillException;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.IOException;
 
@@ -17,40 +17,36 @@ public class BoardTest {
     @Test
     public void boardConstructor2Players() throws NotToRefillException, IOException {
         board = new Board(2);
-        int[][] t2Board = new int[9][9];
         for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
                 if      ((i<4 && j<3 && !(i==3 && j==2)) ||
                         (i<3 && j>4 && !(i==2 && j==5)) ||
                         (i>5 && j<4 && !(i==6 && j==3)) ||
                         (i>4 && j>5 && !(i==5 && j==6)) ||
-                        (i==0 || i==8 || j==0 || j==8)) {
-                    t2Board[i][j] = 0;
-                    assertSame(board.getTiles()[i][j], Tile.BLOCKED);
-                }
-                else {
-                    t2Board[i][j] = 1;
+                        (i==0 || i==8 || j==0 || j==8))
+
+                    assertTrue(board.getTiles()[i][j] == Tile.BLOCKED);
+
+                else
                     assertFalse(board.getTiles()[i][j].isFree());
-                }
+
             }
         }
     }
     @Test
     public void boardConstructor3Players() throws NotToRefillException, IOException {
         board = new Board(3);
-        int[][] t3Board = new int[9][9];
         for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
                 if      ((i<4 && j<3 && !(i==3 && j==2) && !(i==2 && j==2)) ||
                         (i<3 && j>4 && !(i==2 && j==5) && !(i==2 && j==6)) ||
                         (i>5 && j<4 && !(i==6 && j==3) && !(i==6 && j==2)) ||
                         (i>4 && j>5 && !(i==5 && j==6) && !(i==6 && j==6)) ||
-                        (i==0 && j==4) || (i==4 && j==0) || (i==4 && j==8) || (i==8 && j==4)) {
-                    t3Board[i][j] = 0;
-                    assertSame(board.getTiles()[i][j], Tile.BLOCKED);
-                }
+                        (i==0 && j==4) || (i==4 && j==0) || (i==4 && j==8) || (i==8 && j==4))
+
+                    assertTrue(board.getTiles()[i][j] == Tile.BLOCKED);
+
                 else {
-                    t3Board[i][j] = 1;
                     assertFalse(board.getTiles()[i][j].isFree());
                 }
             }
@@ -66,7 +62,7 @@ public class BoardTest {
                         (i>5 && j<4 && !(i==6 && j==3) && !(i==6 && j==2) && !(i==7 && j==3)) ||
                         (i>4 && j>5 && !(i==5 && j==6) && !(i==6 && j==6)) && !(i==5 && j==7)))
                 {
-                    assertEquals(board.getTiles()[i][j], Tile.BLOCKED);
+                    assertTrue(board.getTiles()[i][j] == Tile.BLOCKED);
                 }
                 else {
                     assertFalse(board.getTiles()[i][j].isFree());
@@ -159,36 +155,31 @@ public class BoardTest {
         int [][] t2_tmp_Board = new int[9][9];
         for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
-                if(!boardGeneric.getTiles()[i][j].isFree())
-                    t2_tmp_Board[i][j] = 1;
-                else
+                if(!boardGeneric.getTiles()[i][j].isFree()) {
+                    if (!(i == 3 && j == 3) && !(i == 4 && j == 4)) {
+                        pickedTile = boardGeneric.pickTile(i, j);
+                        t2_tmp_Board[i][j] = -1;
+                    }
+                    else
+                        t2_tmp_Board[i][j] = 1;
+                } else
                     t2_tmp_Board[i][j] = 0;
             }
         }
 
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
-                if(!boardGeneric.getTiles()[i][j].isFree() && !(i==3 && j==3) && !(i==4 && j==4)) {
-                    pickedTile = boardGeneric.pickTile(i, j);
-                    t2_tmp_Board[i][j] = -1;
-                }
-            }
-        }
-        Tile[][] beforeRefillTiles = new Tile[9][9];
-        for(int i=0; i<9; i++)
-            for(int j=0; j<9; j++)
-                beforeRefillTiles[i][j] = boardGeneric.getTiles()[i][j];
+        Tile[] beforeRefillTiles = new Tile[2];
+        beforeRefillTiles[0] = boardGeneric.getTiles()[3][3];
+        beforeRefillTiles[1] = boardGeneric.getTiles()[4][4];
 
-        try{
-            boardGeneric.refill();
-        } catch (NotToRefillException e){}
+        boardGeneric.refill();
+
+        assertTrue(boardGeneric.getTiles()[3][3] == beforeRefillTiles[0]);
+        assertTrue(boardGeneric.getTiles()[4][4] == beforeRefillTiles[1]);
 
         for(int i=0; i<9; i++){
             for (int j=0; j<9; j++){
                 if(t2_tmp_Board[i][j]==-1)
                     assertFalse(boardGeneric.getTiles()[i][j].isFree());
-                if(t2_tmp_Board[i][j]==1)
-                    assertTrue(boardGeneric.getTiles()[i][j] == beforeRefillTiles[i][j]);
             }
         }
 
